e838611629159b84028846bc998cde9fdf2d54c8
Ivan Laver <ereyzer142@gmail.com>
2021-11-30 22:26:28 +0200

    add carrear for style in drags

--- src/components/Сhecker/Checker.js
+++ src/components/Сhecker/Checker.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import style from './Checker.module.css'
+import React from "react";
+import style from "./Checker.module.css";
 
-const Checker = ({darkTeam, isDragStart, isDragEnd}) => {
-
-
-   return <div draggable='true' onDragEnd={isDragEnd} onDragStart={isDragStart} className={`${style.Checker} ${darkTeam ? style.Dark : style.White}` }></div>
-}
+const Checker = ({ darkTeam, isDragStart, isDragEnd }) => {
+  isDragStart = isDragStart(style);
+  isDragEnd = isDragEnd(style);
+  return (
+    <div
+      draggable="true"
+      onDragEnd={isDragEnd}
+      onDragStart={isDragStart}
+      className={`${style.Checker} ${darkTeam ? style.Dark : style.White}`}
+    ></div>
+  );
+};
 
 export default Checker;
--- src/helpers/drags.js
+++ src/helpers/drags.js
@@ -1,43 +1,37 @@
 let currentItem = null;
 
-export const isDragStart = (ev) => {
+export const isDragStart = (style) => (ev) => {
   currentItem = ev.target;
-    ev.target.classList.add(`gavno`);
-    setTimeout(()=>{
-        ev.target.classList.add(`hide`)}, 0)
-  
-  };
- export const isDragEnd = (ev) => {
-    ev.target.classList.remove(`gavno`);
-    ev.target.classList.remove(`hide`);
-  };
+  ev.target.classList.add(style.gavno);
+  setTimeout(() => {
+    ev.target.classList.add(style.hide);
+  }, 0);
+};
+export const isDragEnd = (style) => (ev) => {
+  ev.target.classList.remove(style.gavno);
+  ev.target.classList.remove(style.hide);
+};
 
+export const onDragEnter = (e) => {
+  console.log(2, e);
 
-  export const onDragEnter = (e) => {
-    console.log(2, e);
+  e.target.classList.add("hovered");
+};
 
-    e.target.classList.add('hovered')
-    
-  };
+export const onDragOver = (e) => {
+  console.log(1, e);
+  e.preventDefault();
+};
 
-  export const onDragOver = (e) => {
-    console.log(1, e);
-   e.preventDefault();
-  };
+export const onDragLeave = (style) => (e) => {
+  console.log(3, e);
+  e.target.classList.remove(hovered.hovered);
+};
 
- 
-  export const  onDragLeave = (e) => {
-    console.log(3, e);
-    e.target.classList.remove('hovered')
-    
-  };
-
-  export const onDrop = (e) => {
-    console.log(4, e);
-    console.log(5, e.target)
-    console.log(6, currentItem)
-    e.target.append(currentItem);
-    e.target.classList.remove('hovered');
-  }
-
-
+export const onDrop = (style) => (e) => {
+  console.log(4, e);
+  console.log(5, e.target);
+  console.log(6, currentItem);
+  e.target.append(currentItem);
+  e.target.classList.remove(hovered.hovered);
+};
